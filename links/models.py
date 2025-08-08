0e4ea36dea01609221d989c6595284f00d3845b3
Rayane NACIB <ray.nacib@gmail.com>
2025-08-08 21:39:10 +0100

    Update models.py

--- links/models.py
+++ links/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 # listings/models.py
 
 class Link(models.Model):
     link_id = models.fields.IntegerField()
-    image =models.ImageField(upload_to='images/links', null=True, blank=True)
+    image = CloudinaryField('image', blank=True, null=True)
     title = models.fields.CharField(max_length=100)
     content = models.fields.CharField(max_length=1000)
     date = models.fields.DateField()
