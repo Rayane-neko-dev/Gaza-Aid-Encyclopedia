5121a513e68d710b7109a622a517dcd1fe600baf
Rayane NACIB <ray.nacib@gmail.com>
2025-08-11 18:24:46 +0100

    Update views.py

--- home/views.py
+++ home/views.py
@@ -4,6 +4,6 @@ from links.models import Link
 
 def home(request):
  articles = Article.objects.all()
- links = Link.objects.all()
+ links = Link.objects.all().order_by('-id')[:4]  
  context = {'articles': articles ,'links' : links }
  return render(request, 'home/home.html', context)
