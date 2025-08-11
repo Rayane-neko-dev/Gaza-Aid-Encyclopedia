128d7202a8662191e9e657be01d550fe2ff2eb37
Rayane NACIB <ray.nacib@gmail.com>
2025-08-11 20:48:58 +0100

    Update models.py

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
-from django.core.validators import MaxLengthValidator
 
 # Create your models here.
 
@@ -8,7 +7,7 @@ class Article(models.Model):
     article_id = models.fields.IntegerField()
     image = CloudinaryField('image', blank=True, null=True)
     title = models.fields.CharField(max_length=100)
-    content = models.TextField(validators=[MaxLengthValidator(100000)])
+    content = models.TextField()
     date = models.fields.DateField()
 
     def __str__(self):
