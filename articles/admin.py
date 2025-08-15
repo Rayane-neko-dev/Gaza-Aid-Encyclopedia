d1eeb2034158c5ccdd328803f09074e4dbe5c328
Rayane NACIB <ray.nacib@gmail.com>
2025-08-15 01:19:50 +0100

    Delete articles directory

--- articles/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from articles.models import Article
-
-class ArticleAdmin(admin.ModelAdmin):
-
- list_display = ('title','date','image') 
-
-admin.site.register(Article, ArticleAdmin)
